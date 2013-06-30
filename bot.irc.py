4cda21b0be7c48de1c479a62d1f04417881c42a7
Kijam Lopez <kijamve@gmail.com>
2013-06-30 12:22:54 -0430

    Version no funcional, se sube solo por respaldo

--- bot.irc.py
+++ bot.irc.py
@@ -4,33 +4,78 @@ import re
 import urllib2
 import json
 from random import choice
-import signal, os, sys
+import os, sys
+import threading 
 
 fpid = os.fork()
 if fpid!=0:
   # Running as daemon now. PID is fpid
   sys.exit(0)
 
-signal.signal(signal.SIGHUP, signal.SIG_IGN)
-
+sys.stdout = open('out.log', 'w+')
+sys.stderr = open('err.log', 'w+')
 HOST="irc.radiognu.org"
 PORT=6667
-NICK="BCuado"
-IDENT="BCuado"
-REALNAME="BCuado"
+NICK="SuperB"
+IDENT="SuperB"
+REALNAME="SuperB"
 CHAN="#radiognu"
 USERS = []
+
+def loadJson(name):
+	lines = ""
+	with open(name, "r") as f:
+		for line in f:
+			lines+= line
+	return json.load(lines) 
+
+entradas_bot = loadJson("entradas_bot.json")
+entradas_user = loadJson("entradas_user.json")
+entradas = loadJson("salidas.json")
+chat_idle = loadJson("chat_idle.json")
+menciones = loadJson("menciones.json")
+resp_ia	= loadJson("respuestas_inteligentes.json")
+salidas_user = loadJson("salidas_user.json")
+cambio_nick = loadJson("cambio_nick.json")
+radiognu = loadJson("radiognu.json")
+
 readbuffer  = ""
 s=socket.socket( )
 s.connect((HOST, PORT))
 s.send("NICK %s\r\n" % NICK)
 s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
 s.send("JOIN :%s\r\n" % CHAN)
+lock = Lock()
 
 def send_msg(msg):
+	lock.acquire()
 	s.send("PRIVMSG %s :%s\r\n" % (CHAN, msg))
+	lock.release()
 	print "se envio: %s" % msg
+	
+class AutoBot(threading.Thread):  
+	def __init__(self, num):  
+	  threading.Thread.__init__(self)  
+	  self.num = num  
 
+	def run(self):  
+	while 1:
+		sleep(10)
+t = AutoBot(1)  
+t.start()  
+
+def getRadioGNU():
+	response = urllib2.urlopen("http://audio.radiognu.org/json.xsl")
+	data = json.load(response) 
+	r1 = data["/radiognu.ogg"]
+	r2 = data["/radiognu2.ogg"]
+	r3 = data["/radiometagnu.ogg"]
+	r4 = data["/radiometagnu2.ogg"]
+	r5 = data["/radiometagnuam.ogg"]
+	r6 = data["/radiognuam.ogg"]
+	escuchas = r1["escuchas"] + r2["escuchas"] + r3["escuchas"] + r4["escuchas"] + r5["escuchas"] + r6["escuchas"]
+	return {"titulo": r3["titulo"], "artista": r3["artista"], "escuchas": escuchas}
+	
 while 1:
 	readbuffer=readbuffer+s.recv(1024)
 	temp=string.split(readbuffer, "\n")
@@ -48,36 +93,38 @@ while 1:
 				m = re.search("([a-zA-Z0-9_-]+)", u)
 				if m:
 					USERS.append(m.group(0))
+			send_msg(choice(entradas_bot) % len(USERS))
 		elif lineSplit[1] == "PRIVMSG":
+			if(nick=="Gnoll") continue  #Evitemos que los bot se respondan entre si, se pueden quedar pegados.
 			m = re.search("(:[^:]+:)(.*)", line)
 			msg = m.group(2).strip()
-			if msg.find("radiognu") != -1:
-				response = urllib2.urlopen("http://audio.radiognu.org/json.xsl")
-				data = json.load(response) 
-				r1 = data["/radiognu.ogg"]
-				r2 = data["/radiognu2.ogg"]
-				r3 = data["/radiometagnu.ogg"]
-				r4 = data["/radiometagnu2.ogg"]
-				r5 = data["/radiometagnuam.ogg"]
-				r6 = data["/radiognuam.ogg"]
-				escuchas = r1["escuchas"] + r2["escuchas"] + r3["escuchas"] + r4["escuchas"] + r5["escuchas"] + r6["escuchas"]
-				send_msg("%s de %s nos esta durmiendo, solo %d personas los escucha :(" % (r3["titulo"], r3["artista"], escuchas))
+			wmsg = msg.split()
 			
-			if msg.find("estoy aburrido") != -1:
-				n = choice(USERS)
-				while n==nick:
-					n = choice(USERS)
+			if msg.find("radiognu") != -1:
+				radio = getRadioGNU()
+				send_msg(choice(radiognu) % (radio["titulo"], radio["artista"], radio["escuchas"]))
+			else:
+				mencion = false
+				for p in wmsg:
+					palabra = re.search("([a-zA-Z0-9_-]+)", p).group(0)
+					if palabra in USERS
+						mencion = palabra
+						break
+				op = []
+				for op in resp_ia
 				send_msg("%s te recomiendo que le escribas a %s, el tambien se aburre mucho en este canal"%(nick, n))
 			print "%s dijo: %s\n" %(nick, msg)
-			wmsg = msg.split()
 		elif lineSplit[1] == "NICK":
 			m = re.search("(:[^:]+:)(.*)", line)
 			msg = m.group(2).strip()
 			USERS.append(msg)
-			USERS.remove(msg)
+			USERS.remove(nick)
+			send_msg(choice(cambio_nick)%(msg, nick))
 		elif lineSplit[1] == "JOIN":
+			send_msg(choice(entradas_user)%(nick, choice(USERS)))
 			USERS.append(nick)
 		elif lineSplit[1] == "QUIT":
+			send_msg(choice(salidas_user)%nick)
 			USERS.remove(nick)
 		else:
 			print "UNK: %s\n" % line
